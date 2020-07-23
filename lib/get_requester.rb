d321ae25a60170e6dae9dd335f28a987bb6320ba
Jackson Reynolds <jwreynold@gmail.com>
2020-07-23 01:32:17 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,15 @@ require 'pry'
 class GetRequester
 
   def initialize(url)
-
+    
   end #initialize
+
+  def get_response_body
+
+  end #get_repsonse_body
+
+  def parse_json
+
+  end #parse_json
 
 end #GetRequester
